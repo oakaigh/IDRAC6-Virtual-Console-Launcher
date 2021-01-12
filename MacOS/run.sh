1049d9dbc97e78b28a520c473d0a32ff3f670690
Jared Pyzynski <jhambersob@gmail.com>
2021-01-11 22:32:47 -0800

    sync

--- MacOS/run.sh
+++ MacOS/run.sh
@@ -3,15 +3,6 @@ read -p 'Host/IP: ' drachost
 read -p 'Port [5900]: ' dracport
 dracport=${dracport:-5900}
 
-nc -z -v -w5 $drachost $dracport
-result1=$?
-
-if [  "$result1" != 0 ]; then
-  echo  "Can't access port: $dracport!"
-  return 1 2>/dev/null
-  exit 1
-fi
-
 FILE=./avctKVM.jar
 if [ ! -f "$FILE" ]; then
     curl -O --insecure --header 'Host: www.example.com' -I https://${drachost}:443/software/avctKVM.jar
--- Windows/run.bat
+++ Windows/run.bat
@@ -5,9 +5,6 @@ set dracpwd=none
 
 set /P drachost="Host/IP: "
 
-:nc -v -n -z -w 3 %drachost% 5900
-:IF %errorlevel%==0 goto cant 
-
 if not exist %mypath%\avctKVM.jar (
 powershell -Command "[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};(New-Object System.Net.WebClient).DownloadFile('https://%drachost%:443/software/avctKVM.jar', '%mypath%\avctKVM.jar')"
 )
@@ -26,9 +23,6 @@ IF %dracpwd% EQU none (
 set dracpwd=calvin
 )
 
+start %mypath%\jre\bin\javaw -cp avctKVM.jar -Djava.library.path=%mypath%\lib com.avocent.idrac.kvm.Main ip=%drachost% kmport=5900 vport=5900 user=%dracuser% passwd=%dracpwd% apcp=1 version=2 vmprivilege=true "helpurl=https://%drachost%:443/help/contents.html"
 
-%mypath%\jre\bin\java -cp avctKVM.jar -Djava.library.path=%mypath%\lib com.avocent.idrac.kvm.Main ip=%drachost% kmport=5900 vport=5900 user=%dracuser% passwd=%dracpwd% apcp=1 version=2 vmprivilege=true "helpurl=https://%drachost%:443/help/contents.html"
-
-:cant
-echo Can´t access port 5900!
 @exit 0
